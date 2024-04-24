284605f3bf1f6a15033101175fe77761fce5623a
CalebPuett <90654428+CalebPuett@users.noreply.github.com>
2024-04-24 13:42:44 -0500

    Placement Restrictions
    Stoped from being able to place in path

--- GiraffeDefenceGame/Assets/Scripts/giraffeStart.cs
+++ GiraffeDefenceGame/Assets/Scripts/giraffeStart.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,30 +8,46 @@ public class giraffeStart : MonoBehaviour
     
     private Vector2 mousePosition;
     public float moveSpeed = 0.1f;
+    public bool col = false;
+    bool stop = false;
     void Start()
     {
        
     }
     // Update is called once per frame
     void  Update()
-    {
-            bool stop = place();
+    {   
+            
+            if(!col){
+             stop = Place();
+            }
             if(!stop){
             mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             transform.position = Vector2.Lerp(transform.position,mousePosition,moveSpeed);
             }
             else{
-                moveSpeed = 0;
-                stop = true;
-            }
-           
+                moveSpeed = 0f;
+            } 
     }
 
-    public bool place(){
+    public bool Place(){
+        
          if(Input.GetKeyDown(KeyCode.Mouse0)){
+            
               return true;
             }
         return false;
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Path"){
+            col = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        col = false;
+    }
+   
 }
